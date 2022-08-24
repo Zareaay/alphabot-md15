23af6f7bf443f5487e1dc06d9a5155c524cc040a
Zareaay <111988220+Zareaay@users.noreply.github.com>
2022-08-24 21:27:02 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,18 +25,18 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Chikabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ HumanOfcོ ×፝֟͜×" //nama kalian
+global.botname = "Keilainaabot-Odོ" //namabot kalian
+global.ownername= "ZerrayKING" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6285772762034'] //premium user
-global.owner = ["6285772762034","6285772762034"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6285772762034" // nomor wa kalian
-global.ownernomerr = "+6285772762034" //nmr wa kalian
+global.premium = ['6289668934792'] //premium user
+global.owner = ["6289668934792","6289668934792"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6289668934792" // nomor wa kalian
+global.ownernomerr = "+6289668934792" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
